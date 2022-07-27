4c5837d04ca13ae3bda8770f22d34493c295c03a
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-07-27 11:06:45 +0530

    NavBar.js File Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,27 +7,9 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3 position-sticky top-0">
         <div className="container-fluid">
           <Link className="nav-link active fw-bold text-light " to="/">
-            <span className="h4">React CRUD Application</span>
+            <p className="h4">React <span className="text-warning">CRUD</span> Application</p>
           </Link>
-          {/* <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
           <div  id="navbarSupportedContent">
-            {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-              <li className="nav-item ms-2">
-                <NavLink className="nav-link  fw-bold text-light" to="/home">
-                  Home
-                </NavLink>
-              </li>
-            </ul> */}
             <div className="d-flex">
               <Link className="btn btn-outline-light" to="/adduser" type="submit">
               <i className="bi bi-person-plus me-2"></i>
