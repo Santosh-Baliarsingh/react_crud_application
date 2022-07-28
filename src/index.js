ae9925803dbc6d773002be860796a152f29904f1
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-07-28 10:56:39 +0530

    index.js File Updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 
