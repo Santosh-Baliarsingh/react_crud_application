a7a54e70f3c2c369c9f91e162ba98a3f9bd0bc7b
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-07-28 10:55:47 +0530

    App.js File Updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import { Routes , Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+import ViewUser from "./components/ViewUser";
 
 
 
@@ -15,6 +17,8 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home/>}></Route>
       <Route exact path="/adduser" element={<AddUser/>}></Route>
+      <Route exact path="/edituser/:id" element={<EditUser/>}></Route>
+      <Route exact path="/viewuser/:id" element={<ViewUser/>}></Route>
     </Routes>
     </>
   );
